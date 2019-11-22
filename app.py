b672c6bb1dcb30bc3adef006cd6f780adb1ee153
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2019-11-22 17:54:37 -0300

    commit 22-11-2019 17.54 - pathlib y la clase Path

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from ecommerce.shipping import calc_shipping
+from pathlib import Path
+import random
+
+
+
 """print("Hello World") #Imprime hola mundo
 print('*'*10) #Imprime un string de 10 concatenaciones de '*'
 x = input("Input :")
@@ -18,10 +24,25 @@ print(output)
 def greet_user(name):
     print(f"Hello {name}")
 
-class Point:
-    private name =
+
+class Person:
+    def __init__(self,aName,aLastName,aAddress):
+        self.__Name = aName
+        self.__LastName = aLastName
+        self.__Address = aAddress
+    def GetName(self):
+        return self.__Name
+
     def move(self):
         print("Move")
 
-    def draw(self):
+    def talk(self):
         print("Draw")
+
+
+for x in range(3):
+    random.random()
+    random.randint(10,20)
+    print(random.choice(["Juan","Pedro","Ana"]))
+
+path = Path()
