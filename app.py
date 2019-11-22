ff136cba4003038bbf640877e060ba8b8b0b82e6
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2019-11-22 01:43:06 -0300

    commit 22-11-2019 hasta video sobre clases y objetos

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
-print("Hello World") #Imprime hola mundo
+"""print("Hello World") #Imprime hola mundo
 print('*'*10) #Imprime un string de 10 concatenaciones de '*'
 x = input("Input :")
-print("hello , " + x)
+print("hello , " + x)"""
+entry = input('>')
+words = entry.split(' ')
+emojis = {
+    ":)": "😊",
+    ":(": "☹"
+
+}
+output = ''
+for word in words:
+    output += emojis.get(word, word) + ' '
+print(output)
+
+
+def greet_user(name):
+    print(f"Hello {name}")
+
+class Point:
+    private name =
+    def move(self):
+        print("Move")
+
+    def draw(self):
+        print("Draw")
