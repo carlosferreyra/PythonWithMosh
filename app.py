09e03168a7943fee7156e91fc1a1ccbd3959a0d1
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2019-11-23 19:22:50 -0300

    commit 23-11-2019 hasta comienzo de machine learning.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from ecommerce.shipping import calc_shipping
 from pathlib import Path
 import random
-
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 """print("Hello World") #Imprime hola mundo
 print('*'*10) #Imprime un string de 10 concatenaciones de '*'
 x = input("Input :")
@@ -44,7 +45,28 @@ for x in range(3):
     random.randint(10,20)
     print(random.choice(["Juan","Pedro","Ana"]))
 """
-path = Path()
+"""path = Path()
 for file in path.glob("*"):
     print(file)
+"""
+""" Project 1: Automation
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+    cell = sheet.cell(1, 1)
 
+    for row in range(2, sheet.max_row+1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+    values = Reference(sheet,
+              min_row=2,
+              max_row=sheet.max_row,
+              min_col=4,
+              max_col=4)
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+    wb.save(filename)
+"""
